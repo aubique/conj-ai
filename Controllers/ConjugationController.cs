337f89177275fc553b136b3870a311def9829a3d
aubique <aubicque@gmail.com>
2025-07-13 10:51:01 +0200

    feat: move to Controllers

---
+++ Controllers/ConjugationController.cs
@@ -0,0 +1,70 @@
+﻿using conj_ai.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.Data;
+using System.Text.Json;
+
+namespace conj_ai.Controllers;
+
+[EnableRateLimiting("ip")]
+[ApiController]
+[Route("api/[controller]")]
+
+public class ConjugationController : ControllerBase
+{
+    [HttpGet("{verb}")]
+    public async Task<IActionResult> Get(string verb, IConfiguration config, CancellationToken ct)
+    {
+        const string prompt = "Conjugate the given French verb in these tenses";
+        var chat = new ChatHistory();
+        var executionSettings = new OpenAIPromptExecutionSettings
+        {
+            ResponseFormat = typeof(MultiTenseFrenchConjugation),
+            Temperature = 0,
+        };
+        var modelId = config["OpenAI:Model"];
+        var endpoint = config["OpenAI:Endpoint"];
+        var apiKey = config["OpenAI:ApiKey"];
+
+        Console.WriteLine(verb);
+    
+        try
+        {
+            List<Exception> exceptions = [];
+            if (modelId is null) exceptions.Add(new Exception("Model is not found"));
+            if (endpoint is null) exceptions.Add(new Exception("Endpoint is not found"));
+            if (apiKey is null) exceptions.Add(new Exception("ApiKey is not found"));
+            if (exceptions.Count > 0) throw new AggregateException(exceptions);
+        }
+        catch (AggregateException aggregateEx)
+        {
+            var errors = aggregateEx.InnerExceptions.Select(e => e.Message).ToList();
+                Console.WriteLine($"Configuration missed: {string.Join(", ", errors)}");
+            return NotFound(new { success = false, errors });
+        }
+    
+        var kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(modelId!, new Uri(endpoint!), apiKey).Build();
+    
+        chat.AddSystemMessage(prompt);
+        chat.AddUserMessage(verb);
+        
+        try
+        {
+            var jsonContent = (await kernel.GetRequiredService<IChatCompletionService>()
+                .GetChatMessageContentAsync(chat, executionSettings, kernel, ct))
+                .Content ?? throw new NoNullAllowedException("JSON Content is invalid");
+        
+            var result = JsonSerializer.Deserialize<MultiTenseFrenchConjugation>(jsonContent);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { success = false, error = ex.Message });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
-﻿using conj_ai.Models;
-using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using System.Data;
-using System.Text.Json;
+﻿using Microsoft.AspNetCore.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
 builder.Services.AddRateLimiter(options =>
 {
     options.AddFixedWindowLimiter("ip", configure =>
@@ -18,6 +13,7 @@ builder.Services.AddRateLimiter(options =>
 });
 
 var app = builder.Build();
+app.MapControllers();
 if (app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
@@ -26,54 +22,5 @@ else
 {
     app.UseRateLimiter();
 }
-
-app.MapGet("/{verb}", async (string verb, IConfiguration config, CancellationToken ct) =>
-{
-    const string prompt = "Conjugate the given French verb in these tenses";
-    var chat = new ChatHistory();
-    var executionSettings = new OpenAIPromptExecutionSettings
-    {
-        ResponseFormat = typeof(MultiTenseFrenchConjugation),
-        Temperature = 0,
-    };
-    var modelId = config["OpenAI:Model"];
-    var endpoint = config["OpenAI:Endpoint"];
-    var apiKey = config["OpenAI:ApiKey"];
-
-    try
-    {
-        List<Exception> exceptions = [];
-        if (modelId is null) exceptions.Add(new Exception("Model is not found"));
-        if (endpoint is null) exceptions.Add(new Exception("Endpoint is not found"));
-        if (apiKey is null) exceptions.Add(new Exception("ApiKey is not found"));
-        if (exceptions.Count > 0) throw new AggregateException(exceptions);
-    }
-    catch (AggregateException aggregateEx)
-    {
-        var errors = aggregateEx.InnerExceptions.Select(e => e.Message).ToList();
-        Console.WriteLine($"Configuration missed: {string.Join(", ", errors)}");
-        return Results.NotFound(new { success = false, errors });
-    }
-
-    var kernel = Kernel.CreateBuilder()
-        .AddOpenAIChatCompletion(modelId!, new Uri(endpoint!), apiKey).Build();
-
-    chat.AddSystemMessage(prompt);
-    chat.AddUserMessage(verb);
-
-    try
-    {
-        var jsonContent = (await kernel.GetRequiredService<IChatCompletionService>()
-            .GetChatMessageContentAsync(chat, executionSettings, kernel, ct))
-            .Content ?? throw new NoNullAllowedException("JSON Content is invalid");
-
-        var result = JsonSerializer.Deserialize<MultiTenseFrenchConjugation>(jsonContent);
-        return Results.Ok(result);
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(new { success = false, error = ex.Message });
-    }
-}).RequireRateLimiting("ip");
 
 app.Run();
